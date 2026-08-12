673cf1e8405d804a5f62470df8f2351a38177fc4
tuanlee-tech <tuanlee.tech@gmail.com>
2026-08-12 22:31:41 +0700

    chore(deps): install and register @davidingplus/vitepress-image-viewer

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,15 @@
+import ImageViewerP from '@davidingplus/vitepress-image-viewer'
 import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import DefaultTheme from 'vitepress/theme'
+
+import '@davidingplus/vitepress-image-viewer/style.css'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    enhanceAppWithTabs(app)
+    enhanceAppWithTabs(app),
+      ImageViewerP(app)
   }
 } satisfies Theme
